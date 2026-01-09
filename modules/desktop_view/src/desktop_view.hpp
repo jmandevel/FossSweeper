5e472cc0f5925f5e9afdd31b3a9c470b890be135
Dan Valcour <newjourneyman@yahoo.com>
2026-01-09 16:48:02 -0500

    Change copyright to Free Software Foundation
    I no longer claim copyright for myself, and am dedicating this project to be a public good copyright by FSF. - Daniel Valcour

--- modules/desktop_view/src/desktop_view.hpp
+++ modules/desktop_view/src/desktop_view.hpp
@@ -1,9 +1,9 @@
-// SPDX-FileCopyrightText: 2022 Daniel Valcour <fosssweeper@gmail.com>
+// SPDX-FileCopyrightText: 2025 Free Software Foundation <licensing@fsf.org>
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 /*
- * Copyright (c) 2022 Daniel Valcour
+ * Copyright (c) 2025 Free Software Foundation <licensing@fsf.org>
  *
  * This file is part of FossSweeper.
  *
